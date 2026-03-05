ea3d7b076d45c93e6cc88c51aefdb195fb4ac145
Kathryn Lenth <klenth@westminsteru.edu>
2026-03-05 11:20:12 -0700

    Added Assignment

--- canvas_api/model/__init__.py
+++ canvas_api/model/__init__.py
@@ -1,3 +1,4 @@
+from .assignment import Assignment
 from .enrollment import Enrollment
 from .grade import Grade
 from .user import User
@@ -5,6 +6,7 @@ from .user import User
 # Models built from <https://developerdocs.instructure.com/services/canvas/resources> on 5 March 2026
 
 __all__ = [
+    'Assignment',
     'Enrollment',
     'Grade',
     'User'
